20f4b5120fdc5fbaa76d741f2c955ad9d95bc9fc
mohammadjavvadmohammadi <149230041+mohammadjavvadmohammadi@users.noreply.github.com>
2023-11-05 08:41:18 -0800

    Update namelist.py

--- namelist.py
+++ namelist.py
@@ -78,6 +78,15 @@ shahr_list = ["sari" , "nowshahr" , "amol" , "babol" , "chalous" ,
 for shahrr in shahr_list :
   print(shahrr)
 
+color_list = ["ghermez" , "abi" , "zard" , "sabz" , "narenji" , "sefid" , "siyah" , "banafsh" , 
+              "zereshki" , "sormei" , "khardel" , "sabz lagani" , "talaiy" , "sokolati" , 
+              "jegari" , "bezh" , "ghahveiy" , "kerem" , "toosi" , "khkestari" , "sharabi" , "sooraty" , "sorkhabi" ,
+              "shiri" , "khaki" , "limooei" , "yashmi" , "nili" , "abi nafti" , "bademjani" , "makhamali" , "gandomi" , "asali" , 
+              "katani" , "noghrei" , "doodi" , "heli" , "abi mahv" , "kakaooeiy" , "mashi" , 
+              "mashi sir" , "kerem sir" , "kahgeli" , "orkide" , "banafsh keder" , "lajevardi" , "fosfori" , "abi keder" , "firouzeei" , "abi asemani" ]
+for colorr in color_list :
+  print(colorr)
+
 
 
 
