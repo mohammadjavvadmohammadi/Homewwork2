d674eac5de685ef7d9b9abb5ee59e8d62f70e82a
mohammadjavvadmohammadi <149230041+mohammadjavvadmohammadi@users.noreply.github.com>
2023-11-05 08:49:22 -0800

    Update ketablist.py

--- ketablist.py
+++ ketablist.py
@@ -10,5 +10,12 @@ ketab_list = [" chenin goft zartosht " , " koori " , " binaei " , " sadsal tanha
 for ketabkhane in ketab_list :
      print(ketabkhane) 
 
+odkolon_list = ["tom ford" , "faboulos" , "tosckan leather" , "belu shannel" , "oniro" , 
+                "invectus" , "amber leather" , "oud wood" , "creed aventus" , "victoria secret" , "savage dior" ,
+                "dior home" , "megamare" , "bakaret rozh" ]
+for atr in odkolon_list :
+  print(atr)
+                
+
 
 
