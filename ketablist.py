286de39c850f66c3f5dc2c620c5a0dcf43293c6b
mohammadjavvadmohammadi <149230041+mohammadjavvadmohammadi@users.noreply.github.com>
2023-11-05 09:45:00 -0800

    Update ketablist.py

--- ketablist.py
+++ ketablist.py
@@ -1,10 +1,16 @@
-ketab_list = [" chenin goft zartosht " , " koori " , " binaei " , " sadsal tanhaei " , " 1984 " ," ghale heyvanat " , " vaghti niche gerist " , 
-               " kimiagar " , " soghoot " , " melat eshg" , " ablah" , " shazdeh koochoolo " , " joz az kol" , " badam " , " man ahmagh " ,
-               " dokhtar porteghali " , " kafsh baz " , " aghayed yek dalghak" , " too " , " nan o sharab ", " -odise " , " otagh " , " tahavoe " , 
-               " iliyad" , " raz divinchi" , " aame pasand " , " hamlet" , " raz " , " ayen zendegi " , " ikigay " , " ghodrat adat " , " irag mirza" , 
+   ketab_list = [" chenin goft zartosht " , " koori " , " binaei " , " sadsal tanhaei " , 
+            " 1984 " ," ghale heyvanat " , " vaghti niche gerist " , 
+               " kimiagar " , " soghoot " , " melat eshg" , " ablah" , " shazdeh koochoolo " ,
+              " joz az kol" , " badam " , " man ahmagh " ,
+               " dokhtar porteghali " , " kafsh baz " , " aghayed yek dalghak" ,
+              " too " , " nan o sharab ", " -odise " , " otagh " , " tahavoe " , 
+               " iliyad" , " raz divinchi" , " aame pasand " , " hamlet" , " raz " , 
+              " ayen zendegi " , " ikigay " , " ghodrat adat " , " irag mirza" , 
                " do gharn sokoot " , " bishoori " , " man pish az to" , " ketab dozd" , 
-              " donyaye soofi " , " maghaze khodkoshi " , " marg ivan ilich " , " jenayat o mokafat " , " piremard  o daraya" , " sang kaghaz gheychi" ,
-              " dastan do shahr " , " salto " , " pas az too " , " badbadak baz " , " ghodrat sokoot " , " adam haye sami " , 
+              " donyaye soofi " , " maghaze khodkoshi " , " marg ivan ilich " , 
+              " jenayat o mokafat " , " piremard  o daraya" , " sang kaghaz gheychi" ,
+              " dastan do shahr " , " salto " , " pas az too " , " badbadak baz " ,
+              " ghodrat sokoot " , " adam haye sami " , 
               " pasokh be tarikh" , " 11 daghighe " , " darman shopenhaver " ]
 
 for ketabkhane in ketab_list :
@@ -16,7 +22,7 @@ odkolon_list = ["tom ford" , "faboulos" , "tosckan leather" , "belu shannel" , "
 for atr in odkolon_list :
   print(atr)
 
-abc_list = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , 
+abc_list = ["a" , "b" , "c" ,d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , 
             "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s"  , "t" , "u" , "v" , 
             "w" , "x" , "Y" , "z" ]
 for alefba in abc :
@@ -30,10 +36,14 @@ for world in keshvar_list :
   print(world)
 
 track_list = ["1-ta tehran" , "2-flex" , "3-gorg o mish" , "4-sturdy" , 
-              "5-paband" , "6-haminam" , "7-memphis" , "8-damage" , "9-ak46" , "10-odd" , "11-balenciaga" , "12-hate" , "13-opps" , "14-cardi" , "15-jomeh" , 
-              "16-shanbeh" , "17-dont" , "18-pile" , "19-kalafegi" , "20-medusa" , "21-red flag" , "22-hardski" , "23-mask" , "24-x" , "25-knock knock" , 
-              "26-lego" , "27-rip" , "28-boof" , "29-qol" , "30-audi" , "31-t" , "32-mbk" , "33-bozdel" , "34-beshmar" , "35-hitman" , "36-jamorant" ,
-              "37-omran" , "38-metgala" , "39-sskol" , "40-mikesham" , "41-kgb" , "42-zarbat" , "43-ynd" , "44-damn things" , "45-lost" , "46-tmac" ,
+              "5-paband" , "6-haminam" , "7-memphis" , "8-damage" , "9-ak46" , "10-odd" , "11-balenciaga" , 
+              "12-hate" , "13-opps" , "14-cardi" , "15-jomeh" , 
+              "16-shanbeh" , "17-dont" , "18-pile" , "19-kalafegi" , "20-medusa" , "21-red flag" , 
+              "22-hardski" , "23-mask" , "24-x" , "25-knock knock" , 
+              "26-lego" , "27-rip" , "28-boof" , "29-qol" , "30-audi" , "31-t" , "32-mbk" , 
+              "33-bozdel" , "34-beshmar" , "35-hitman" , "36-jamorant" ,
+              "37-omran" , "38-metgala" , "39-sskol" , "40-mikesham" , "41-kgb" , 
+              "42-zarbat" , "43-ynd" , "44-damn things" , "45-lost" , "46-tmac" ,
               "47-3am" , "48-skiki" , "49-jibitz" , "50-runaway" ]
 for playlist in track_list :
   print(playlist)
