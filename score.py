5ddfe321c724753579e9613e6624ff78e2b0395a
mohammadjavvadmohammadi <149230041+mohammadjavvadmohammadi@users.noreply.github.com>
2023-11-05 08:42:33 -0800

    Update score.py

--- score.py
+++ score.py
@@ -10,3 +10,34 @@ password_2 = input("Enter password again : ")
 while password_1 == password_2 :
     print ( " correct " )
     break
+
+
+s1 = int(input("Please enter the grade of your Farsi : "))
+s2 = int(input("Please enter your grade of narm afzar : "))
+s3 = int(input("Please enter your grade of tarbiat badani : "))
+s4 = int(input("Please enter your grade barname nevisi : "))
+s5 = int(input("Please enter your grade of php : "))
+s6 = int(input("Please enter your grade of python: "))
+s7 = int(input("Please enter your grade of wordpress :"))
+s8 = int(input("Please enter your grade of css : "))
+s9 = int(input("Please enter your grade of english : "))
+s10 = int(input("Please enter your grade of azamayeshgah : "))
+
+average = (s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9 + s10) /10
+while average != 20 :
+    print ( "error")
+    break
+s1 = int(input("Please enter the grade of your Farsi : "))
+s2 = int(input("Please enter your grade of narm afzar : "))
+s3 = int(input("Please enter your grade of tarbiat badani : "))
+s4 = int(input("Please enter your grade barname nevisi : "))
+s5 = int(input("Please enter your grade of php : "))
+s6 = int(input("Please enter your grade of python "))
+s7 = int(input("Please enter your grade of wordpress : "))
+s8 = int(input("Please enter your grade of css : "))
+s9 = int(input("Please enter your grade of english : "))
+s10 = int(input("Please enter your grade of azamayeshgah : "))
+average = (s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9 + s10) /10
+while average == 20 : 
+    print ( "bicycle" )
+    break
